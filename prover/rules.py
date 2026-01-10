42e1ff73dbfa8f234b1317e47c418c619176f12b
Firstname Surname <z.wang.logic@gmail.com>
2026-01-10 17:15:43 +0100

    Update rules.py
    rule_id has been implemented

--- prover/rules.py
+++ prover/rules.py
@@ -1,18 +1,21 @@
 from syntax import *
 
-# list of rules in LabIKs
 # each rule is a fuction of type Sequent -> list[Sequent]
+
+def rule_id(G : Sequent) -> list[Sequent]:
+    for f1 in G.formulas:
+        match f1:
+            case LFormula(label = l1, formula = Prop(p1), polarity = Polarity.IN):
+                for f2 in G.formulas:
+                    match f2:
+                        case LFormula(label = l2, formula = Prop(p2), polarity = Polarity.OUT) if l1 == l2 and p1 == p2:
+                            return []
+    return []
+
+
+
 RULES = [
     rule_id,
-    rule_bot,
-    rule_and_in,
-    rule_and_out,
-    rule_or_in,
-    rule_or_out,
-    rule_imp_in,
-    rule_imp_out,
-    rule_rf,
-    rule_tr,
 ]
 
 def apply_rules(G : Sequent) -> list[Sequent]:
@@ -24,3 +27,14 @@ def apply_rules(G : Sequent) -> list[Sequent]:
         
     return [] # no rule can be applied
     
+# test rule_id
+x = Label("x")
+A = Prop("A")
+
+G = Sequent([], [
+    LFormula(x, A, Polarity.IN),
+    LFormula(x, A, Polarity.OUT)
+])
+
+print("conclusion:", G)
+print("premise:", apply_rules(G))
