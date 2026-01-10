67c5a57b6c559e9fde0b02a5d2b7ca8181c0e26c
Firstname Surname <z.wang.logic@gmail.com>
2026-01-10 15:16:13 +0100

    syntax without modalities

---
+++ prover/syntax.py
@@ -0,0 +1,153 @@
+from dataclasses import dataclass
+
+# Formulas
+
+class Formula:
+   pass
+
+
+@dataclass
+class Prop(Formula):
+    p: str
+
+    def __str__(self):
+        return self.p
+
+
+@dataclass
+class Bot(Formula):
+    def __str__(self):
+        return "bot"
+
+
+@dataclass
+class And(Formula):
+    left: Formula
+    right: Formula
+
+    def __str__(self):
+        return f"{self.left} and {self.right}"
+
+
+@dataclass
+class Or(Formula):
+    left: Formula
+    right: Formula
+
+    def __str__(self):
+        return f"{self.left} or {self.right}"
+
+
+@dataclass
+class Imp(Formula):
+    left: Formula
+    right: Formula
+
+    def __str__(self):
+        return f"{self.left} -> {self.right}"
+
+
+#  test   
+A = Prop("A")
+B = Prop("B")
+
+f = Imp(A, Or(Bot(), And(A, B)))
+
+print(f)       
+
+
+# Labels
+
+
+@dataclass
+class Label:
+    name : str
+    
+    def __str__(self):
+        return f"{self.name}"
+
+# Polarity          
+
+
+class Polarity:
+    IN = "in"
+    OUT = "out"
+
+
+# Labelled formulas
+
+
+@dataclass
+class LFormula:
+    label : Label
+    formula : Formula
+    polarity : Polarity
+
+    def __str__(self):
+        return f"{self.label} : {self.formula} {self.polarity}"
+    
+
+    
+#test 
+
+x = Label("x")
+p = Prop("P")
+
+f2 = LFormula(x, p, Polarity.IN)
+print(f2)
+
+
+# Preorder relation
+
+@dataclass
+class Preorder:
+    left : Label
+    right : Label
+
+    def __str__(self):
+        return f"{self.left} <= {self.right}"
+    
+
+# test
+x = Label("x")
+y = Label("y")
+
+f = Preorder(x,y)
+print(f)
+
+
+# Sequent
+
+
+@dataclass
+class Sequent:
+    relations : list[Preorder]
+    formulas : list[LFormula]
+
+    def to_string(self):
+        s = ""
+
+        if len(self.relations) > 0:
+            last = self.relations[-1]
+            for r in self.relations:
+                s += r.__str__()
+                if r != last:
+                    s += ", "
+
+        s = s + ", "
+
+        if len(self.formulas) > 0:
+            last = self.formulas[-1]
+            for f in self.formulas:
+                s += f.__str__()
+                if f != last:
+                    s += ", "
+
+        return s
+    
+    def __str__(self):
+        return self.to_string()
+    
+# test
+s = Sequent([Preorder(x,y)],[A,f])
+print(s)
