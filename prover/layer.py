c631e1ed2fd42fb4842e1cf0f62bcffea20e79ef
Firstname Surname <z.wang.logic@gmail.com>
2026-01-17 11:52:23 +0100

    Update layer.py
    rename

--- prover/layer.py
+++ prover/layer.py
@@ -1,5 +1,5 @@
 from syntax import *
 
-# without modality, layer(G) = [{x} | x is label appearing in G}]
-def layer(G : Sequent):
+# without modality, all_layers(G) = [{x} | x is label appearing in G}]
+def all_layers(G : Sequent):
     return [{x} for x in all_labels(G)]
