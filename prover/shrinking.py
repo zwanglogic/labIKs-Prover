f37178dbb9bd4e757b65533a3816e79e31be45c5
Firstname Surname <z.wang.logic@gmail.com>
2026-01-26 20:36:09 +0100

    Create shrinking.py
    Implement several notions from definitions 5.10 and 5.11.

---
+++ prover/shrinking.py
@@ -0,0 +1,104 @@
+from lift import *
+
+
+def compute_largest_simulation(G: Sequent) -> set[tuple[Label, Label]]:
+    """
+    Definition 5.10 (simulation)
+
+    Compute the largest simulation. Implemented by using the Greatest Fixed Point Algorithm.
+    """
+    change = True
+    S = set()
+
+    for x in all_labels(G):
+        for y in all_labels(G):
+            if are_equivalent_labels(G, x, y):
+                S.add((x, y))
+
+    while change == True:
+        change = False
+
+        for x, y in list(S):
+            for y_hat in all_labels(G):
+                found = False
+                if Relation(y, y_hat) not in G.modal_relations:
+                    continue
+                for x_hat in all_labels(G):
+                    if (Relation(x, x_hat) in G.modal_relations) and ((x_hat, y_hat) in S):
+                        found = True
+                        break
+                if not found:
+                    S.remove((x, y))
+                    change = True
+                    break
+
+    return S
+
+
+def uniform_substitution(G: Sequent, old: Label, new: Label) -> Sequent:
+    """
+    Uniformly substituting an old label for a new label in G.
+    """
+    new_relations = set()
+    for r in G.relations:
+        left = new if r.left == old else r.left
+        right = new if r.right == old else r.right
+        new_relations.add(Preorder(left, right))
+
+    new_modal_relations = set()
+    for r in G.modal_relations:
+        left = new if r.left == old else r.left
+        right = new if r.right == old else r.right
+        new_modal_relations.add(Relation(left, right))
+
+    new_formulas = set()
+    for f in G.formulas:
+        label = new if f.label == old else f.label
+        new_formulas.add(LFormula(label, f.formula, f.polarity))
+
+    return Sequent(
+        frozenset(new_relations),
+        frozenset(new_modal_relations),
+        frozenset(new_formulas)
+    )
+
+
+def are_siblings(G: Sequent, x: Label, y: Label) -> bool:
+    """
+    Definition 5.11
+
+    x|y iff there is a label z s.t. z R x and z R y.
+    """
+    for z in all_labels(G):
+        if Relation(z, x) in G.modal_relations and Relation(z, y) in G.modal_relations:
+            return True
+
+    return False
+
+
+def is_redundant(G: Sequent, y: Label, S: set[tuple[Label, Label]]) -> bool:
+    """
+    Definition 5.11
+
+    Here we can simply use the largest simulation.
+    """
+    for x in all_labels(G):
+        if x == y:
+            continue
+
+        if are_siblings(G, x, y) and (x, y) in S:
+            return True
+
+    return False
+
+
+def is_shrunk(G: Sequent, S: set[tuple[Label, Label]]) -> bool:
+    """
+    Definition 5.11
+
+    Here we can simply use the largest simulation.
+    """
+    for x in all_labels(G):
+        if is_redundant(G, x, S):
+            return False
+    return True
